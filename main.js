01d5a436c4286768220792ac62dea35f2e4ad41d
Brian Tang <80483610+briantang1@users.noreply.github.com>
2021-06-10 19:40:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '-';
+
+client.once('ready', () => {
+    console.log('Brozty Bot is online!');
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command == 'ping'){
+        message.channel.send('pong!');
+    } else if (command == 'youtube' || command == 'yt'){
+        message.channel.send('https://www.youtube.com/brozty');
+    } else if(command == 'karu'){
+        message.channel.send('https://www.twitch.tv/karuzki');
+    } else if(command == 'chaos'){
+        message.channel.send('https://www.twitch.tv/starvader_chaos');
+    } else if(command == 'sladex'){
+        message.channel.send('https://www.youtube.com/channel/UCiBKRnNBv_3PscyVtacKa3w');
+    }
+});
+
+
+client.login('ODUyNjkxMDUwMjc5NDY5MDY2.YMKgbA.xKnRHVBmuiHOTp89WitduLk2UzU'); //end of file
